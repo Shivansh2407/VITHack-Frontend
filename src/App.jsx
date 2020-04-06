ac2fc25a150f67754988e525f83713b715e8e079
HelixW <shreyas.2000@hotmail.com>
2020-04-06 12:34:32 +0530

    Default colors and fonts setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 
 const App = () => (
   <div className='h-screen flex justify-center items-center'>
-    <div className='w-1/2 h-12 flex justify-center items-center bg-red-800 text-white rounded-lg shadow-lg text-center'>
+    <div className='w-1/2 h-12 flex justify-center items-center bg-gray-200 text-white rounded-lg shadow-lg text-center'>
       I am using tailwind.css
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   theme: {
-    extend: {},
+    screens: {
+      sm: '870px',
+      md: '1140px',
+      lg: '1600px',
+    },
+    extend: {
+      backgroundColor: {
+        'hack-blue': '#1F4BD8',
+        'hack-yellow': '#FFBD3B',
+        'hack-pink': '#EA3883',
+        'hack-green': '#BDE321',
+      },
+      textColor: {
+        'hack-blue': '#1F4BD8',
+        'hack-yellow': '#FFBD3B',
+        'hack-pink': '#EA3883',
+        'hack-green': '#BDE321',
+      },
+    },
   },
-  variants: {},
+  variants: { display: ['responsive', 'hover', 'focus'] },
   plugins: [],
-}
+};
