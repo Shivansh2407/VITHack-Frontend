02d89f826b78f8bc0c47c6c287695a0cb2e9ac0e
HelixW <shreyas.2000@hotmail.com>
2020-04-06 11:43:19 +0530

    Setup Airbnb styleguide using ESLint

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 const App = () => (
-	<div className='h-screen flex justify-center items-center'>
-		<div className='w-1/2 h-12 flex justify-center items-center bg-red-800 text-white rounded-lg shadow-lg text-center'>
-			I am using tailwind.css
-		</div>
-	</div>
+  <div className='h-screen flex justify-center items-center'>
+    <div className='w-1/2 h-12 flex justify-center items-center bg-red-800 text-white rounded-lg shadow-lg text-center'>
+      I am using tailwind.css
+    </div>
+  </div>
 );
 
 export default App;
