675531bae7af9ac5ae2f541debffdf5fb4fce2f1
HelixW <shreyas.2000@hotmail.com>
2020-04-07 16:45:54 +0530

    Enabled shadow for navbar

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Components
 import { ReactComponent as VitHackLogo } from '../../vectors/VitHackLogo.svg';
 import NavLink from './NavLink';
 
 const Navbar = () => {
+  const [scrollPosition, setScrollPosition] = useState(
+    document.documentElement.scrollTop
+  );
+
   const navLinks = [
     { id: 1, text: 'Blog', url: '#' },
     { id: 2, text: 'Help us Out', url: '#' },
@@ -12,8 +16,18 @@ const Navbar = () => {
     { id: 4, text: 'Contact Us', url: '#' },
   ];
 
+  useEffect(() =>
+    window.addEventListener('scroll', () =>
+      setScrollPosition(document.documentElement.scrollTop)
+    )
+  );
+
   return (
-    <nav className='w-full text-center bg-white text-black h-24 flex justify-center items-center fixed top-0 justify-evenly z-10'>
+    <nav
+      className={`w-full text-center bg-white text-black h-24 flex justify-center items-center fixed top-0 justify-evenly z-10 ${
+        scrollPosition > 10 && 'shadow-lg'
+      }`}
+    >
       {navLinks.slice(0, 2).map((navLink) => (
         <div key={navLink.id}>
           <NavLink key={NavLink.id} text={navLink.text} url={navLink.url} />
