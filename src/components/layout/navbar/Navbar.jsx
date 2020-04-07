39efb7971d2845161a9bc013309182f3e34c32e3
HelixW <shreyas.2000@hotmail.com>
2020-04-07 18:31:15 +0530

    Preloader structure laid out

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -5,9 +5,8 @@ import { ReactComponent as VitHackLogo } from '../../vectors/VitHackLogo.svg';
 import NavLink from './NavLink';
 
 const Navbar = () => {
-  const [scrollPosition, setScrollPosition] = useState(
-    document.documentElement.scrollTop
-  );
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   const navLinks = [
     { id: 1, text: 'Blog', url: '/#' },
@@ -16,38 +15,53 @@ const Navbar = () => {
     { id: 4, text: 'Contact Us', url: '/#contact-us' },
   ];
 
-  useEffect(() =>
+  useEffect(() => {
     window.addEventListener('scroll', () =>
       setScrollPosition(document.documentElement.scrollTop)
-    )
-  );
+    );
+    const timer = setTimeout(() => setLoading(false), 4000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <nav
-      className={`w-full text-center bg-white text-black h-24 flex justify-center items-center fixed top-0 z-10 ${
-        scrollPosition > 10 && 'shadow-lg'
-      }`}
-    >
-      <div className='w-1/3 flex justify-evenly items-center'>
-        {navLinks.slice(0, 1).map((navLink) => (
-          <div key={navLink.id}>
-            <NavLink key={NavLink.id} text={navLink.text} url={navLink.url} />
-          </div>
-        ))}
+    <>
+      {/* Preloader starts  */}
+      <div
+        className={`w-screen h-screen fixed inset-0 bg-white z-20 flex justify-center items-center text-4xl ${
+          !loading && 'hidden'
+        }`}
+      >
+        <span className='text-hack-blue'>36</span>&nbsp;Hours,&nbsp;
+        <span className='text-hack-yellow'>1</span>&nbsp;Hack,&nbsp;
+        <span className='text-hack-pink'>1</span>&nbsp;Networking Session
       </div>
-      <div className='w-1/3 flex justify-center items-center'>
-        <a href='/#' className='cursor-pointer'>
-          <VitHackLogo />
-        </a>
-      </div>
-      <div className='w-1/3 flex justify-evenly items-center'>
-        {navLinks.slice(1).map((navLink) => (
-          <div key={navLink.id}>
-            <NavLink key={NavLink.id} text={navLink.text} url={navLink.url} />
-          </div>
-        ))}
-      </div>
-    </nav>
+      {/* Preloader ends  */}
+      <nav
+        className={`w-full text-center bg-white text-black h-24 flex justify-center items-center fixed top-0 z-10 ${
+          scrollPosition > 10 && 'shadow-lg'
+        }`}
+      >
+        <div className='w-1/3 flex justify-evenly items-center'>
+          {navLinks.slice(0, 1).map((navLink) => (
+            <div key={navLink.id}>
+              <NavLink key={NavLink.id} text={navLink.text} url={navLink.url} />
+            </div>
+          ))}
+        </div>
+        <div className='w-1/3 flex justify-center items-center'>
+          <a href='/#' className='cursor-pointer'>
+            <VitHackLogo />
+          </a>
+        </div>
+        <div className='w-1/3 flex justify-evenly items-center'>
+          {navLinks.slice(1).map((navLink) => (
+            <div key={navLink.id}>
+              <NavLink key={NavLink.id} text={navLink.text} url={navLink.url} />
+            </div>
+          ))}
+        </div>
+      </nav>
+    </>
   );
 };
 
