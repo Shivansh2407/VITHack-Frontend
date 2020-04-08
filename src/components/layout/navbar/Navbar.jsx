fe88f0583f7564aca1fdb47f429e66e4d2bfa62f
HelixW <shreyas.2000@hotmail.com>
2020-04-09 00:16:49 +0530

    Preloader animation completed using GSAP

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
 
 // Components
-import { ReactComponent as VitHackLogo } from '../../vectors/VitHackLogo.svg';
 import NavLink from './NavLink';
 
 const Navbar = () => {
-  const [loading, setLoading] = useState(true);
-
   const navLinks = [
     { id: 1, text: 'Blog', url: '/#' },
     // { id: 2, text: 'Help us Out', url: '#' },
@@ -14,24 +12,54 @@ const Navbar = () => {
     { id: 4, text: 'Contact Us', url: '#page1' },
   ];
 
+  const preLoaderScreen1 = useRef(null);
+  const logo = useRef(null);
+  const preLoaderScreen2 = useRef(null);
+
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 4000);
-    return () => clearTimeout(timer);
+    const tl = gsap.timeline();
+    tl.to(preLoaderScreen1.current, {
+      opacity: 0,
+      display: 'none',
+      duration: 1,
+      delay: 3,
+    })
+      .to(logo.current, { scale: 0.5, top: '47px', duration: 2, delay: 0.5 })
+      .to(preLoaderScreen2.current, {
+        opacity: 0,
+        display: 'none',
+        duration: 1,
+      });
   }, []);
 
   return (
     <>
       {/* Preloader starts  */}
       <div
-        className={`w-screen h-screen fixed inset-0 bg-white z-20 flex justify-center items-center text-4xl ${
-          !loading && 'hidden'
-        }`}
+        ref={preLoaderScreen1}
+        className='w-screen h-screen fixed inset-0 bg-white z-40 flex justify-center items-center text-4xl'
       >
         <span className='text-hack-blue'>36</span>&nbsp;Hours,&nbsp;
         <span className='text-hack-yellow'>1</span>&nbsp;Hack,&nbsp;
         <span className='text-hack-pink'>1</span>&nbsp;Networking Session
       </div>
-      {/* Preloader ends  */}
+      <div
+        ref={preLoaderScreen2}
+        className='w-screen h-screen fixed inset-0 bg-white z-20 flex justify-center items-center'
+      />
+      <a
+        href='/#'
+        ref={logo}
+        className='cursor-pointer absolute z-30'
+        style={{ top: '50%', left: '50%', transform: 'translate(-50%,-50%)' }}
+      >
+        <img
+          ref={logo}
+          src={`${process.env.PUBLIC_URL}/assets/images/VitHackLogo.png`}
+          alt='Hack Logo'
+        />
+      </a>
+      {/* preLoaderScreen1 ends  */}
       <nav className='w-full text-center bg-white text-black h-24 flex justify-center items-center fixed top-0 z-10 shadow-md'>
         <div className='w-1/3 flex justify-evenly items-center'>
           {navLinks.slice(0, 1).map((navLink) => (
@@ -40,11 +68,7 @@ const Navbar = () => {
             </div>
           ))}
         </div>
-        <div className='w-1/3 flex justify-center items-center'>
-          <a href='/#' className='cursor-pointer'>
-            <VitHackLogo />
-          </a>
-        </div>
+        <div className='w-1/3 flex justify-center items-center' />
         <div className='w-1/3 flex justify-evenly items-center'>
           {navLinks.slice(1).map((navLink) => (
             <div key={navLink.id}>
