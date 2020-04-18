4518fbcdc980f97e8dce3e854a54ce999019d10d
Shivansh Srivastava <42182955+Shivansh2407@users.noreply.github.com>
2020-04-18 10:04:55 +0530

    Update TrackCard.jsx

--- src/components/trackSection/TrackCard.jsx
+++ src/components/trackSection/TrackCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const TrackCard = ({ name }) => {
   return (
-    <div className='mx-16 my-2 w-40 h-40 flex-shrink-0 flex flex-col justify-evenly items-center'>
+    <div className='mx-12 my-2 w-40 h-40 flex-shrink-0 flex flex-col justify-evenly items-center'>
       <div className='border-2 border-black rounded-full w-20 h-20' />
       <div>{name}</div>
     </div>
