8ace8a38b82508bcb4a957f61ccfe5ec83555505
HelixW <shreyas.2000@hotmail.com>
2020-04-18 16:16:43 +0530

    Removed track and team sections

--- src/components/aboutSection/AboutSection.jsx
+++ src/components/aboutSection/AboutSection.jsx
@@ -1,41 +1,27 @@
 import React from 'react';
 
 // Components
-import Heading from '../layout/Heading';
-import SubSection from '../layout/SubSection';
-import BulbOMan from './BulbOMan';
 import PreviousFigures from './PreviousFigures';
+import SubSection from '../layout/SubSection';
+import Heading from '../layout/Heading';
+import { ReactComponent as BulbOMan } from '../vectors/BulbOMan.svg';
+
+// Data
+import { ABOUT_SUB_SECTIONS } from '../../dataStore';
 
 const AboutSection = () => {
-  const subSections = [
-    {
-      id: 1,
-      heading: 'About VIT',
-      text:
-        'Vellore Institute of Technology, Vellore, is a profound engineering university in India. VIT is well known for its rich diversity in nationality and culture in its students. It encourages aspiring innovators and researchers to work towards technical development. A technical fest, namely, graVITas is conducted here to portray these technical aficionados on an official forum with experts for advice and inspiration. Being a celebrated college it demands a humungous footfall in graVITas.',
-      showLess: true,
-    },
-    {
-      id: 2,
-      heading: 'Why VIT Hack?',
-      text:
-        'VIT Hack aims to provide an arena for participants to contribute their ideas as solutions to real-world problems. Being a global event, it aims to gain from the unification of various cultures leading to various solutions. Problem statements will be driven towards social causes to help channelize the participants’ thoughts into a revolutionary solution. VIT Hack focuses on bringing leading industry experts to help ignite innovation. All that we wish is you bring your best ideas, your confidence and your willingness to have a great time.',
-      showLess: true,
-    },
-  ];
-
   return (
     <section className='section text-hack-black flex flex-col justify-center items-center mx-auto relative overflow-hidden'>
       <div className='w-screen h-20' />
       <div className='flex justify-start items-center'>
         <section className='pl-35'>
           <Heading>About</Heading>
-          {subSections.map((subSection) => (
+          {ABOUT_SUB_SECTIONS.map((subSection) => (
             <SubSection
-              key={subSection.id}
-              text={subSection.text}
-              heading={subSection.heading}
               showLess={subSection.showLess}
+              heading={subSection.heading}
+              text={subSection.text}
+              key={subSection.id}
             />
           ))}
         </section>
--- src/dataStore.js
+++ src/dataStore.js
@@ -7,15 +7,15 @@ const LICENSE_KEY = 'OPEN-SOURCE-GPLV3-LICENSE';
 const SECTION_ANCHORS = [
   'home',
   'about',
-  'tracks',
+  // 'tracks',
   'help',
   'benefactors',
-  'team',
+  // 'team',
   'contact',
   'subscribe',
 ];
 
-// Navlinks:
+// Nav Links:
 const NAV_LINKS = [
   { id: 1, text: 'Blog', url: '/#home', anchor: 'home' },
   { id: 2, text: 'Help us Out', url: '/#help', anchor: 'help' },
@@ -23,4 +23,22 @@ const NAV_LINKS = [
   { id: 4, text: 'Contact Us', url: '/#contact', anchor: 'contact' },
 ];
 
-export { SECTION_ANCHORS, LICENSE_KEY, NAV_LINKS };
+// About Section
+const ABOUT_SUB_SECTIONS = [
+  {
+    id: 1,
+    heading: 'About VIT',
+    text:
+      'Vellore Institute of Technology, Vellore, is a profound engineering university in India. VIT is well known for its rich diversity in nationality and culture in its students. It encourages aspiring innovators and researchers to work towards technical development. A technical fest, namely, graVITas is conducted here to portray these technical aficionados on an official forum with experts for advice and inspiration. Being a celebrated college it demands a humungous footfall in graVITas.',
+    showLess: true,
+  },
+  {
+    id: 2,
+    heading: 'Why VIT Hack?',
+    text:
+      'VIT Hack aims to provide an arena for participants to contribute their ideas as solutions to real-world problems. Being a global event, it aims to gain from the unification of various cultures leading to various solutions. Problem statements will be driven towards social causes to help channelize the participants’ thoughts into a revolutionary solution. VIT Hack focuses on bringing leading industry experts to help ignite innovation. All that we wish is you bring your best ideas, your confidence and your willingness to have a great time.',
+    showLess: true,
+  },
+];
+
+export { ABOUT_SUB_SECTIONS, SECTION_ANCHORS, LICENSE_KEY, NAV_LINKS };
