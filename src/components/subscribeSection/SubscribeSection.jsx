3a6fed6bcba29c20b79547e5adb0e174847c7c86
HelixW <shreyas.2000@hotmail.com>
2020-04-19 12:49:02 +0530

    Subscribe section is now responsive

--- src/components/subscribeSection/SubscribeSection.jsx
+++ src/components/subscribeSection/SubscribeSection.jsx
@@ -9,9 +9,9 @@ import { FOOTER_LINKS } from '../../DataStore';
 
 const SubscribeSection = () => (
   <section className='section text-hack-black flex flex-col justify-center items-center relative overflow-hidden cursor-default mx-auto'>
-    <div className='w-screen h-32' />
+    <div className='hidden sm:block w-screen h-32' />
     <section className='flex justify-center items-center -mt-8'>
-      <div className='w-1/2 text-2xl font-bold flex items-center text-center mb-16 px-20'>
+      <div className='sm:w-2/3 md:w-1/2 text-2xl font-bold flex items-center text-center mb-16 px-8 sm:px-20'>
         Be a part of the movement and receive regular updates by subscribing to
         our newsletter!
       </div>
@@ -19,7 +19,7 @@ const SubscribeSection = () => (
     <div className='flex flex-col justify-center items-center -mt-8'>
       <input
         type='email'
-        className='rounded py-2 px-6 border border-hack-blue w-1/4 border-2 border-hack-blue mb-8'
+        className='rounded py-2 px-6 border border-hack-blue sm:w-1/4 border-2 border-hack-blue mb-8'
         placeholder='Email Here'
         name='email'
       />
@@ -31,10 +31,10 @@ const SubscribeSection = () => (
       </button>
     </div>
     <br />
-    <footer className='flex justify-center absolute bottom-0 w-screen mb-4'>
+    <footer className='flex flex-row flex-wrap justify-center absolute bottom-0 w-screen mb-4 px-8 sm:p-0'>
       {FOOTER_LINKS.map((link) => (
         <a
-          className='text-hack-blue hover:underline mx-4'
+          className='text-hack-blue hover:underline text-center mx-4'
           href={link.link}
           key={link.id}
         >
