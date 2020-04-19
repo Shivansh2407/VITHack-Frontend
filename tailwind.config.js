7c064400f5c8757ef913511f1abaeaef3363ade1
HelixW <shreyas.2000@hotmail.com>
2020-04-20 00:23:54 +0530

    Fixed fonts and navbar height

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
       md: '1140px',
       lg: '1600px',
     },
+    fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      tiny: '.875rem',
+      base: '1.125rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
     extend: {
       backgroundColor: {
         'hack-blue': '#008DFF',
@@ -30,6 +44,9 @@ module.exports = {
         48: '12rem',
         80: '20rem',
         96: '24rem',
+      },
+      height: {
+        18: '4.5rem',
       },
     },
   },
