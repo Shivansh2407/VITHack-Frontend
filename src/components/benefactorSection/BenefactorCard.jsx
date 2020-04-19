e05628452a4d0b3361d16388ad277c6e34e8223a
HelixW <shreyas.2000@hotmail.com>
2020-04-19 23:10:09 +0530

    Benefactor icon size shrunk

--- src/components/benefactorSection/BenefactorCard.jsx
+++ src/components/benefactorSection/BenefactorCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const BenefactorCard = ({ imageSrc }) => (
-  <div className='w-40 h-40 flex justify-center items-center mx-4 sm:mx-16 md:mx-24'>
+  <div className='w-20 sm:w-40 h-20 sm:h-40 flex justify-center items-center mx-8 sm:mx-16 md:mx-24'>
     <img className='w-full' src={imageSrc} alt='Logo' />
   </div>
 );
