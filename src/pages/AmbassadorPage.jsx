5d17ebbdf44418657cd89232ae3bd67d4c62f4d3
HelixW <shreyas.2000@hotmail.com>
2020-05-01 23:56:56 +0530

    Illustration added

--- src/pages/AmbassadorPage.jsx
+++ src/pages/AmbassadorPage.jsx
@@ -5,6 +5,7 @@ import SubSection from '../components/layout/SubSection';
 import Heading from '../components/layout/Heading';
 import Navbar from '../components/layout/navbar/Navbar';
 import HamburgerMenu from '../components/layout/navbar/HamburgerMenu';
+import { ReactComponent as SeeA } from '../components/vectors/seeA.svg';
 
 // Data
 import { CA_HELP_TEXT, CA_FAQ_SUB_SECTIONS, CA_FAQ } from '../DataStore';
@@ -14,49 +15,52 @@ const AmbassadorPage = () => {
     <>
       <HamburgerMenu />
       <Navbar />
-      <section className='section flex flex-col justify-center items-center mx-auto relative overflow-hidden cursor-default sm:text-base'>
-        <div className='hidden sm:block w-screen h-20' />
-        <div>
+      <div className='relative'>
+        <section className='section flex flex-col justify-center items-center mx-auto relative overflow-hidden cursor-default sm:text-base'>
+          <div className='hidden sm:block w-screen h-20' />
+          <div>
+            <section className='px-8 sm:px-35 md:pl-35 relative'>
+              <div className='text-hack-purple'>
+                <Heading>Campus Ambassador</Heading>
+              </div>
+              <p className='w-full md:w-5/12 mb-6 mt-8'>{CA_HELP_TEXT.text}</p>
+              <div className='flex justify-start'>
+                <button
+                  className='bg-hack-blue cursor-not-allowed opacity-50 text-white rounded font-bold capitalize py-2 px-8'
+                  type='button'
+                >
+                  Coming Soon
+                </button>
+              </div>
+            </section>
+          </div>
+        </section>
+        <section className='section text-hack-black flex flex-col justify-center items-center mx-auto relative overflow-hidden cursor-default sm:text-base'>
+          <div className='hidden sm:block w-screen h-20' />
           <section className='px-8 sm:px-35 md:pl-35'>
-            <div className='text-hack-purple'>
-              <Heading>Campus Ambassador</Heading>
-            </div>
-            <p className='w-full md:w-5/12 mb-6 mt-8'>{CA_HELP_TEXT.text}</p>
-            <div className='flex justify-center sm:justify-start'>
-              <button
-                className='bg-hack-blue cursor-not-allowed opacity-50 text-white rounded font-bold capitalize py-2 px-8'
-                type='button'
-              >
-                Coming Soon
-              </button>
-            </div>
+            {CA_FAQ_SUB_SECTIONS.map((subSection) => (
+              <SubSection
+                heading={subSection.heading}
+                text={subSection.text}
+                key={subSection.id}
+                purple
+              />
+            ))}
           </section>
-        </div>
-      </section>
-      <section className='section text-hack-black flex flex-col justify-center items-center mx-auto relative overflow-hidden cursor-default sm:text-base'>
-        <div className='hidden sm:block w-screen h-20' />
-        <section className='px-8 sm:px-35 md:pl-35'>
-          {CA_FAQ_SUB_SECTIONS.map((subSection) => (
-            <SubSection
-              heading={subSection.heading}
-              text={subSection.text}
-              key={subSection.id}
-              purple
-            />
-          ))}
+          <header className='text-sm-sub-heading sm:text-sub-heading font-bold text-hack-purple w-full px-8 sm:px-35'>
+            FAQs
+          </header>
+          <section className='px-8 sm:px-35 w-full'>
+            {CA_FAQ.map((subSection) => (
+              <div key={subSection.id} className='w-full md:w-7/12'>
+                <h4 className='font-bold mt-8'>{subSection.heading}</h4>
+                <div className='my-4'>{subSection.text}</div>
+              </div>
+            ))}
+          </section>
         </section>
-        <header className='text-sm-sub-heading sm:text-sub-heading font-bold text-hack-purple w-full px-8 sm:px-35'>
-          FAQs
-        </header>
-        <section className='px-8 sm:px-35 w-full'>
-          {CA_FAQ.map((subSection) => (
-            <div key={subSection.id} className='w-full md:w-7/12'>
-              <h4 className='font-bold mt-8'>{subSection.heading}</h4>
-              <div className='my-4'>{subSection.text}</div>
-            </div>
-          ))}
-        </section>
-      </section>
+        <SeeA />
+      </div>
     </>
   );
 };
