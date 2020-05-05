ddd03f91b47710e9c1d6cff04dcf9b2272ca45f1
HelixW <shreyas.2000@hotmail.com>
2020-05-05 22:03:23 +0530

    Footer location fixed

--- src/components/landingSection/LandingSection.jsx
+++ src/components/landingSection/LandingSection.jsx
@@ -13,7 +13,7 @@ const LandingSection = () => (
     className='section bg-white h-screen flex flex-col overflow-hidden cursor-default sm:text-base relative'
     id='home'
   >
-    {/* <RegisterGirl /> */}
+    <RegisterGirl />
     <div className='h-full flex justify-center items-center'>
       <div className='w-1/3 hidden md:block'>
         <img src={LandingGroup1} alt='Code Man' className='ml-20' />
@@ -23,7 +23,7 @@ const LandingSection = () => (
         <img src={LandingGroup2} alt='Code Man' className='mr-20' />
       </div>
     </div>
-    {/* <CodeMan /> */}
+    <CodeMan />
     <LandingFooter />
   </section>
 );
