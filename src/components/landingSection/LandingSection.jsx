bd776f8f0c232cdc04a3ab7342a0d68e24820316
HelixW <shreyas.2000@hotmail.com>
2020-05-05 20:36:04 +0530

    New illustrations for landing screen on mobile viewport

--- src/components/landingSection/LandingSection.jsx
+++ src/components/landingSection/LandingSection.jsx
@@ -1,37 +1,31 @@
 import React from 'react';
 
 // Components
-// import { ReactComponent as LandingGroup1 } from '../vectors/LandingGroup1.svg';
-// import { ReactComponent as LandingGroup2 } from '../vectors/LandingGroup2.svg';
 import LandingFooter from './LandingFooter';
 import HeroCaption from './HeroCaption';
-// import { ReactComponent as VitHackLogo } from '../vectors/VitHackLogo.svg';
 import LandingGroup1 from '../vectors/LandingGroup1.gif';
 import LandingGroup2 from '../vectors/LandingGroup2.gif';
+import { ReactComponent as CodeMan } from '../vectors/codeMan.svg';
+import { ReactComponent as RegisterGirl } from '../vectors/registerGirl.svg';
 
 const LandingSection = () => (
   <section
-    className='section bg-white h-screen flex flex-col overflow-hidden cursor-default sm:text-base'
+    className='section bg-white h-screen flex flex-col overflow-hidden cursor-default sm:text-base relative'
     id='home'
   >
-    {/* <a
-      className='sm:hidden cursor-pointer absolute z-20'
-      href='/#home'
-      style={{ top: '25px', left: '50%', transform: 'translateX(-50%)' }}
-    >
-      <VitHackLogo />
-    </a> */}
+    <div>
+      <RegisterGirl />
+    </div>
     <div className='h-full flex justify-center items-center'>
       <div className='w-1/3 hidden md:block'>
         <img src={LandingGroup1} alt='Code Man' className='ml-20' />
-        {/* <LandingGroup1 /> */}
       </div>
       <HeroCaption />
       <div className='w-1/3 hidden md:block'>
         <img src={LandingGroup2} alt='Code Man' className='mr-20' />
-        {/* <LandingGroup2 /> */}
       </div>
     </div>
+    <CodeMan />
     <LandingFooter />
   </section>
 );
