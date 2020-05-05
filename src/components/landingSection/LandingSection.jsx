e5ce4cf89b0edcc105ad9c58971d953fa1c87284
HelixW <shreyas.2000@hotmail.com>
2020-05-05 21:57:39 +0530

    Testing build

--- src/components/landingSection/LandingSection.jsx
+++ src/components/landingSection/LandingSection.jsx
@@ -13,9 +13,7 @@ const LandingSection = () => (
     className='section bg-white h-screen flex flex-col overflow-hidden cursor-default sm:text-base relative'
     id='home'
   >
-    <div>
-      <RegisterGirl />
-    </div>
+    {/* <RegisterGirl /> */}
     <div className='h-full flex justify-center items-center'>
       <div className='w-1/3 hidden md:block'>
         <img src={LandingGroup1} alt='Code Man' className='ml-20' />
@@ -25,7 +23,7 @@ const LandingSection = () => (
         <img src={LandingGroup2} alt='Code Man' className='mr-20' />
       </div>
     </div>
-    <CodeMan />
+    {/* <CodeMan /> */}
     <LandingFooter />
   </section>
 );
