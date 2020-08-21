d76b72ad0f54c0731908888a1184302d09726828
HelixW <shreyas.2000@hotmail.com>
2020-08-22 03:01:05 +0530

    footer updated
    Signed-off-by: HelixW <shreyas.2000@hotmail.com>

--- src/components/landingSection/LandingFooter.jsx
+++ src/components/landingSection/LandingFooter.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
+import React from 'react'
 
 // Components
-import { ReactComponent as GravitasLogo } from '../vectors/graVITas20Logo.svg';
-import { ReactComponent as VitLogoBlue } from '../vectors/VitLogoBlue.svg';
+import { ReactComponent as GravitasLogo } from '../vectors/graVITas20Logo.svg'
+import { ReactComponent as VitLogoBlue } from '../vectors/VitLogoBlue.svg'
 
 const LandingFooter = () => (
   <footer className='w-full h-24 flex flex-col sm:flex-row justify-center items-center absolute bottom-0 text-hack-black z-0 mb-20 sm:m-0'>
-    <div className='sm:mr-6 lowercase'>in collaboration with</div>
-    <div className='flex justify-center items-center'>
-      <a href='http://vit.ac.in' target='_blank' rel='noopener noreferrer'>
-        <VitLogoBlue />
-      </a>
-      &nbsp;&nbsp;&nbsp;&nbsp;
+    <div className='w-full flex justify-center sm:justify-between items-center px-8 sm:px-35'>
+      <VitLogoBlue />
       <a
-        href='https://vit.ac.in/gravitas/'
+        href='http://gravitas.vit.ac.in'
         target='_blank'
         rel='noopener noreferrer'
+        className='text-hack-blue text-sub-heading font-bold hover:underline transform translate-x-5 hidden sm:block'
       >
-        <GravitasLogo />
+        GraVITas '20 Website
       </a>
+      <span className='sm:hidden'>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+      <GravitasLogo />
     </div>
   </footer>
-);
+)
 
-export default LandingFooter;
+export default LandingFooter
